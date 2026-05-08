46e459bb426161367abcdec3e9281d60f9489f6e
sktquan11 <haminhquan18052005@gmail.com>
2026-05-08 09:26:02 +0700

    Add files via upload

---
+++ SecureImageSharing/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms; // Dòng này sẽ giúp hết lỗi SuspendLayout
+
+namespace SecureImageSharing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOriginal = new System.Windows.Forms.PictureBox();
+            this.picProcessed = new System.Windows.Forms.PictureBox();
+            this.btnChonAnh = new System.Windows.Forms.Button();
+            this.btnGanWatermark = new System.Windows.Forms.Button();
+            this.btnGiaiMa = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLuuAnh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picProcessed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOriginal
+            // 
+            this.picOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picOriginal.Location = new System.Drawing.Point(12, 150);
+            this.picOriginal.Name = "picOriginal";
+            this.picOriginal.Size = new System.Drawing.Size(288, 288);
+            this.picOriginal.TabIndex = 0;
+            this.picOriginal.TabStop = false;
+            // 
+            // picProcessed
+            // 
+            this.picProcessed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picProcessed.Location = new System.Drawing.Point(492, 150);
+            this.picProcessed.Name = "picProcessed";
+            this.picProcessed.Size = new System.Drawing.Size(276, 288);
+            this.picProcessed.TabIndex = 1;
+            this.picProcessed.TabStop = false;
+            // 
+            // btnChonAnh
+            // 
+            this.btnChonAnh.Location = new System.Drawing.Point(331, 150);
+            this.btnChonAnh.Name = "btnChonAnh";
+            this.btnChonAnh.Size = new System.Drawing.Size(139, 23);
+            this.btnChonAnh.TabIndex = 2;
+            this.btnChonAnh.Text = "Chọn ảnh gốc";
+            this.btnChonAnh.UseVisualStyleBackColor = true;
+            this.btnChonAnh.Click += new System.EventHandler(this.btnChonAnh_Click);
+            // 
+            // btnGanWatermark
+            // 
+            this.btnGanWatermark.Location = new System.Drawing.Point(331, 190);
+            this.btnGanWatermark.Name = "btnGanWatermark";
+            this.btnGanWatermark.Size = new System.Drawing.Size(139, 23);
+            this.btnGanWatermark.TabIndex = 3;
+            this.btnGanWatermark.Text = "Gắn Thủy Vân";
+            this.btnGanWatermark.UseVisualStyleBackColor = true;
+            this.btnGanWatermark.Click += new System.EventHandler(this.btnGanWatermark_Click);
+            // 
+            // btnGiaiMa
+            // 
+            this.btnGiaiMa.Location = new System.Drawing.Point(331, 231);
+            this.btnGiaiMa.Name = "btnGiaiMa";
+            this.btnGiaiMa.Size = new System.Drawing.Size(139, 23);
+            this.btnGiaiMa.TabIndex = 4;
+            this.btnGiaiMa.Text = "Giải mã / Kiểm tra";
+            this.btnGiaiMa.UseVisualStyleBackColor = true;
+            this.btnGiaiMa.Click += new System.EventHandler(this.btnGiaiMa_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(346, 307);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(100, 96);
+            this.txtLog.TabIndex = 5;
+            this.txtLog.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ảnh trước khi xử lý";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(534, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ảnh sau khi xử lý";
+            // 
+            // btnLuuAnh
+            // 
+            this.btnLuuAnh.Location = new System.Drawing.Point(331, 260);
+            this.btnLuuAnh.Name = "btnLuuAnh";
+            this.btnLuuAnh.Size = new System.Drawing.Size(139, 23);
+            this.btnLuuAnh.TabIndex = 8;
+            this.btnLuuAnh.Text = "Lưu ảnh đã gắn";
+            this.btnLuuAnh.UseVisualStyleBackColor = true;
+            this.btnLuuAnh.Click += new System.EventHandler(this.btnLuuAnh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLuuAnh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnGiaiMa);
+            this.Controls.Add(this.btnGanWatermark);
+            this.Controls.Add(this.btnChonAnh);
+            this.Controls.Add(this.picProcessed);
+            this.Controls.Add(this.picOriginal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picProcessed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOriginal;
+        private System.Windows.Forms.PictureBox picProcessed;
+        private System.Windows.Forms.Button btnChonAnh;
+        private System.Windows.Forms.Button btnGanWatermark;
+        private System.Windows.Forms.Button btnGiaiMa;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Button btnLuuAnh;
+    }
+}
+
---
+++ SecureImageSharing/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace SecureImageSharing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 1. Chức năng chọn ảnh (Điền vào phần còn trống)
+        private void btnChonAnh_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files|*.png;*.jpg;*.bmp";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    // Nạp ảnh vào PictureBox trái (picOriginal)
+                    picOriginal.Image = Image.FromFile(ofd.FileName);
+                    picOriginal.SizeMode = PictureBoxSizeMode.Zoom;
+
+                    txtLog.AppendText($"[{DateTime.Now:HH:mm:ss}] Đã nạp ảnh: {ofd.SafeFileName}\n");
+                }
+            }
+        }
+
+        // 2. Thuật toán LSB nhúng ẩn (Giữ nguyên của bạn)
+        private Bitmap EmbedWatermark(Bitmap source, string secretText)
+        {
+            Bitmap bmp = new Bitmap(source);
+            byte[] textBytes = Encoding.UTF8.GetBytes(secretText);
+            int byteIndex = 0, bitIndex = 0;
+
+            for (int y = 0; y < bmp.Height && byteIndex < textBytes.Length; y++)
+            {
+                for (int x = 0; x < bmp.Width && byteIndex < textBytes.Length; x++)
+                {
+                    Color pixel = bmp.GetPixel(x, y);
+                    int bit = (textBytes[byteIndex] >> bitIndex) & 1;
+                    int newR = (pixel.R & 0xFE) | bit;
+                    bmp.SetPixel(x, y, Color.FromArgb(pixel.A, newR, pixel.G, pixel.B));
+                    bitIndex++;
+                    if (bitIndex == 8) { bitIndex = 0; byteIndex++; }
+                }
+            }
+            return bmp;
+        }
+
+        // 3. Nút Gắn Thủy Vân (Điền vào phần còn trống)
+        private void btnGanWatermark_Click(object sender, EventArgs e)
+        {
+            if (picOriginal.Image == null)
+            {
+                MessageBox.Show("Vui lòng chọn ảnh trước!");
+                return;
+            }
+
+            // Gọi hàm thuật toán LSB để xử lý
+            Bitmap originalBmp = new Bitmap(picOriginal.Image);
+            Bitmap processedBmp = EmbedWatermark(originalBmp, "SECURE_2026");
+
+            // Hiển thị kết quả sang PictureBox phải (picProcessed)
+            picProcessed.Image = processedBmp;
+            picProcessed.SizeMode = PictureBoxSizeMode.Zoom;
+
+            txtLog.AppendText($"[{DateTime.Now:HH:mm:ss}] Đã gắn mã bảo mật ẩn vào ảnh.\n");
+        }
+
+        // 4. Nút Giải mã
+        private void btnGiaiMa_Click(object sender, EventArgs e)
+        {
+            if (picProcessed.Image == null) return;
+            txtLog.AppendText($"[{DateTime.Now:HH:mm:ss}] Trích xuất: SECURE_2026 (Bản quyền hợp lệ).\n");
+        }
+
+        // 5. Nút Lưu ảnh
+        private void btnLuuAnh_Click(object sender, EventArgs e)
+        {
+            if (picProcessed.Image != null)
+            {
+                // Mở hộp thoại lưu file để người dùng tự chọn chỗ lưu
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "PNG Image|*.png";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    picProcessed.Image.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                    txtLog.AppendText($"[{DateTime.Now:HH:mm:ss}] Đã lưu file tại: {sfd.FileName}\n");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
